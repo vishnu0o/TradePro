06112240a09e4a2dafe7fd1fa9831bf3f8797eb9
lilacVishnu <vishnu.v@lilacinfotech.com>
2024-09-12 23:57:58 +0530

    course FindOne api completed

--- src/controller/courseController.js
+++ src/controller/courseController.js
@@ -11,13 +11,31 @@ export const courseFindController = asyncHandler(async (req, res) => {
     const limit = 5;
     const skipValue = parseInt(pageNumber) * limit;
     const findCourse = await Courses.find({}).limit(limit).skip(skipValue);
-    res
-      .status(200)
-      .json({
-        message: "Course find successfully",
-        data: findCourse,
-        status: true
-      });
+    res.status(200).json({
+      message: "Course find successfully",
+      data: findCourse,
+      status: true
+    });
+  } catch (error) {
+    console.log(error, "error");
+    res.status(500).json({ message: "Something went wrong", data: error });
+  }
+});
+
+// @desc    course findOne
+// @route   get /api/course/findOneCourse
+// @access  user
+
+export const courseFindOneController = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const findCourse = await Courses.findOne({_id:id });
+    res.status(200).json({
+      message: "Course find successfully",
+      data: findCourse,
+      status: true
+    });
   } catch (error) {
     console.log(error, "error");
     res.status(500).json({ message: "Something went wrong", data: error });
--- src/routes/courseRoute.js
+++ src/routes/courseRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { courseFindController } from "../controller/courseController.js";
+import { courseFindController, courseFindOneController } from "../controller/courseController.js";
 import auth from "../middleware/auth.js";
 const router = express.Router();
-router.route("/findCourse").get(courseFindController);
+router.route("/findCourse").get(auth,courseFindController);
+router.route("/findOneCourse").get(auth,courseFindOneController);
+
 
 export default router;
